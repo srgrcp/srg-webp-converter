665cb4186eb40f7d3db6f6b747c556b34f08048e
srgrcp <srgrcp@gmail.com>
2022-02-28 12:54:14 -0500

    bugfix: Home screen disappears

--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -1,13 +1,10 @@
 import { InputFile } from '@shared/input-file';
 import { useRouter } from 'next/router';
 import { useCallback } from 'react';
-import { useRecoilValue } from 'recoil'
 import DropFilesArea from '../components/drop-files-area'
 import { useOpenFileDialog, useSetInputFiles } from '../store/callbacks';
-import { selectInputFilesLength } from '../store/selectors';
 
 export default function Home() {
-  const inputFilesCount = useRecoilValue(selectInputFilesLength);
   const router = useRouter();
   const openFileDialog = useOpenFileDialog();
   const setInputFiles = useSetInputFiles();
@@ -36,7 +33,7 @@ export default function Home() {
   return (
     <div className='container mx-auto py-5 px-4 md:px-8 lg:px-12 2xl:px-16 h-screen flex items-center'>
       <div className='flex flex-grow flex-col items-center w-full'>
-        { !inputFilesCount && <DropFilesArea handleOpenFiles={openFileDialogAndRedirect} setInputFiles={setInputFilesAndRedirect} />}
+        <DropFilesArea handleOpenFiles={openFileDialogAndRedirect} setInputFiles={setInputFilesAndRedirect} />
       </div>
     </div>
   )
